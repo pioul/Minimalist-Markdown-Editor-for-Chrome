d80edace4b3553f413bab6053d621a37a865332a
Philippe Masset <pioul06+github@gmail.com>
2015-07-03 19:15:58 +0200

    Update build
    - Use updated Babel Gulp plugin
    - Now transpile all JS files to ES5, except external libs

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,30 @@
 var gulp = require("gulp"),
 	del = require("del"),
-	to5 = require("gulp-6to5");
+	babel = require("gulp-babel");
 
 gulp.task("clean", function(c) {
 	del("dist/**", c);
 });
 
 // Transpile to ES5
-// JS files inside app-shared/ are currently authored in ES5, hence don't need this
-gulp.task("6to5", ["clean"], function() {
-	return gulp.src("src/js/*.js", { base: "src/" })
-		.pipe(to5())
+// Input: All JS files but those under libs/
+gulp.task("to-ES5", ["clean"], function() {
+	return gulp.src([
+		"src/**/*.js",
+		"!src/**/libs/**"
+	], { base: "src/" })
+		.pipe(babel())
 		.pipe(gulp.dest("dist/"));
 });
 
 // Simply copy the rest of the files from src/ to dist/
+// Input: All files but JS files that are not under libs/
 gulp.task("copy", ["clean"], function() {
 	return gulp.src([
-			"src/**",
-			"!src/js/*.js"
-		], { base: "src/" })
+		"src/**",
+		"!src/**/!(libs)/*.js" // With this glob, JS files must be directly under libs/ (change the glob if nesting folders)
+	], { base: "src/" })
 		.pipe(gulp.dest("dist/"));
 });
 
-gulp.task("default", ["6to5", "copy"]);
+gulp.task("default", ["to-ES5", "copy"]);
